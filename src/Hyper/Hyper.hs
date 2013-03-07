633b1021f5c14aa2e89ae0131f8420c94fe673ec
jason <jason@johnson-net.ch>
2013-03-07 15:04:08 +0100

    Changed Hyper to use new version of parse method

--- src/Hyper/Hyper.hs
+++ src/Hyper/Hyper.hs
@@ -1,25 +1,10 @@
 module Main where
 
 import Hyper.Options
-import System.Environment (getArgs)
-import Control.Monad (when)
+import System.Environment (getArgs, getProgName)
 
 main :: IO ()
 main = do
-    (args, ports) <- getArgs >>= parse
-    when (DatabaseConfig `elem` args) $ putStrLn "Got Database Config!"
-    when (MultiSite `elem` args) $ putStrLn "MultiSite enabled!"
-    when (ResourcePerRequest `elem` args) $ putStrLn "ResourcePerRequest enabled!"
-    showSSL args
-    showConfigFile args
-    putStrLn $ "Ports are: " ++ concat ports
-    putStrLn "And we're done!"
-        where
-                showSSL [] = putStrLn "No SSL options set"
-                showSSL (SSL Nothing:_) = putStrLn "SSL option set with default port"
-                showSSL (SSL (Just port):_) = putStrLn $ "SSL option set with port: " ++ show port
-                showSSL (_:args) = showSSL args
-                
-                showConfigFile [] = putStrLn "No config file option"
-                showConfigFile (Config file:_) = putStrLn $ "Config file configured at: " ++ file
-                showConfigFile (_:args) = showConfigFile args
+        progName <- getProgName
+        config <- getArgs >>= parse progName
+        putStrLn $ show config
