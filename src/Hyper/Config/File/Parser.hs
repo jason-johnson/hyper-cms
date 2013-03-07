539653d11fea6f6fa405263dbe4c99a8528655bd
jason <jason@johnson-net.ch>
2013-03-07 23:49:23 +0100

    Created Config.File.Parser for parsing config files

---
+++ src/Hyper/Config/File/Parser.hs
@@ -0,0 +1,75 @@
+module Hyper.Config.File.Parser
+(
+  parseConfigFile
+, parseInput
+)
+where
+
+import Control.Applicative
+import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+import qualified Text.Parsec.Token as P
+import Text.Parsec.Language (emptyDef)
+
+data CValue = CString String
+            | CInt Integer
+            | CDouble Double
+            | CBool Bool
+              deriving (Eq, Ord, Show)
+                          
+data CEntry = CEntry String CValue deriving (Show)
+
+data CSection = CSection String [CEntry] deriving (Show)
+
+p_configuration :: CharParser () [CSection]
+p_configuration = spaces *> many1 p_section
+            
+p_between :: Char -> CharParser () a -> Char -> CharParser () a
+p_between left parser right =
+    between (char left <* spaces) (char right) (parser <* spaces)
+            
+p_section :: CharParser () (CSection)
+p_section = CSection <$> p_between '[' (many1 ch) ']' <*> (spaces *> p_entries) <?> "section"
+        where ch = satisfy (`notElem` "]\"\\")
+
+p_entries :: CharParser () [CEntry]
+p_entries = many entry <?> "entries"
+        where
+                entry = CEntry <$> fieldName <*> (spaces *> char '=' *> spaces *> p_value)
+                fieldName = (:) <$> letter <*> many fieldChar
+                fieldChar = letter <|> digit <|> oneOf "-_"
+    
+p_value :: CharParser () CValue
+p_value = value <* spaces
+  where value = CString <$> p_string
+            <|> p_int_or_float
+            <|> CBool <$> p_bool
+            <?> "entry value"
+
+p_bool :: CharParser () Bool
+p_bool = True <$ string "true"
+     <|> False <$ string "false"
+     
+-- TODO: negative numbers not support, is this needed?
+p_int_or_float :: CharParser () CValue
+p_int_or_float = p <$> naturalOrFloat
+    where
+        p (Right x)    = CDouble x
+        p (Left x)     = CInt x
+        naturalOrFloat = P.naturalOrFloat lexer
+        lexer          = P.makeTokenParser emptyDef
+
+p_string :: CharParser () String
+p_string = between (char '\"') (char '\"') (many ch)
+    where ch = char '\\' *> p_escape <|> satisfy (`notElem` "\"\\")
+
+p_escape :: CharParser () Char
+p_escape = choice (zipWith decode "\\\"/" "\\\"/")
+    where decode c r = r <$ char c
+
+parseInput :: SourceName -> [Char] -> Either ParseError [CSection]
+parseInput file input = parse p_configuration file input
+
+parseConfigFile :: FilePath -> IO (Either ParseError [CSection])
+parseConfigFile file = do
+                        input <- readFile file
+                        return $ parseInput file input
