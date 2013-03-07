dbe8e7dfe0f7045daaedc893723e54fbbf3367e7
jason <jason@johnson-net.ch>
2013-03-07 23:50:20 +0100

    Changed Hyper to use loadConfiguration

--- src/Hyper/Hyper.hs
+++ src/Hyper/Hyper.hs
@@ -1,10 +1,11 @@
 module Main where
 
 import Hyper.Options
+import Hyper.Config (loadConfiguration)
 import System.Environment (getArgs, getProgName)
 
 main :: IO ()
 main = do
         progName <- getProgName
-        config <- getArgs >>= parse progName
+        config <- getArgs >>= parse progName >>= loadConfiguration
         putStrLn $ show config
