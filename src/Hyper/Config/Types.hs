9237b54a9205dfb96824ae3bc6f475241dbc57d9
jason <jason@johnson-net.ch>
2013-03-09 20:45:12 +0100

    Config.Types: Added monoid instances for my configuration types

--- src/Hyper/Config/Types.hs
+++ src/Hyper/Config/Types.hs
@@ -6,7 +6,8 @@ module Hyper.Config.Types
 )
 where
 
-import           Data.Map (Map)
+import           Data.Map    (Map, unionWith)
+import           Data.Monoid
 
 data SiteConfiguration = SiteConfiguration {
           root           :: FilePath           -- TODO: For now we'll simply do file routing but we might let clients override this later
@@ -14,10 +15,10 @@ data SiteConfiguration = SiteConfiguration {
         , passthrough    :: [String]
         , cacheDirectory :: FilePath
 }
-          deriving (Show)
+          deriving (Eq, Show)
 
 data ConfigurationStore = Database | ConfigFile String
-        deriving (Show)
+        deriving (Eq, Show)
 
 data Configuration = Configuration {
           configurationStore          :: ConfigurationStore
@@ -29,4 +30,31 @@ data Configuration = Configuration {
         , configurationDefaultSite    :: SiteConfiguration
         , configurationSites          :: Map String SiteConfiguration -- TODO: The key should be using some kind of Host name that Yesod uses.
 }
-        deriving (Show)
+        deriving (Eq, Show)
+
+instance Monoid SiteConfiguration where
+    a `mappend` b = SiteConfiguration (root a           `mset` root b)
+                                      (index a          `mset` index b)
+                                      (passthrough a    `mset` passthrough b)
+                                      (cacheDirectory a `mset` cacheDirectory b)
+    mempty = SiteConfiguration mempty mempty mempty mempty
+
+instance Monoid ConfigurationStore where
+    mappend = mset
+    mempty = ConfigFile mempty
+
+instance Monoid Configuration where
+    a `mappend` b = Configuration (configurationStore a `mappend` configurationStore b)
+                                  (configurationSinglePort b)
+                                  (configurationPorts a `mset` configurationPorts b)
+                                  (configurationSSlPort b)
+                                  (configurationMultiSite b)
+                                  (configurationResourcePerReq b)
+                                  (configurationDefaultSite a `mappend` configurationDefaultSite b)
+                                  (unionWith mset (configurationSites a) (configurationSites b))
+    mempty = Configuration mempty False mempty Nothing False True mempty mempty
+
+mset :: (Eq a, Monoid a) => a -> a -> a
+mset a b | b == mempty = a
+         | otherwise = b
+
