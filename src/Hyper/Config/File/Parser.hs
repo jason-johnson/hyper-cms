b95aeeb0e4c975f41c4100c33db8ac3e5fa7e168
jason <jason@johnson-net.ch>
2013-03-10 21:55:58 +0100

    Config.File.Parser: Completely rewritten to use sensible parsing and state (comments)
    Still not done but a good check point

--- src/Hyper/Config/File/Parser.hs
+++ src/Hyper/Config/File/Parser.hs
@@ -7,147 +7,187 @@ where
 
 import           Control.Applicative
 import qualified Data.Map                      as M
-import           Data.Monoid                   (mappend, mempty)
+import           Data.Monoid                   (mappend)
 import           Hyper.Config.Types
-import           Text.Parsec.Language          (emptyDef)
-import qualified Text.Parsec.Token             as P
 import           Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
-import Debug.Trace(trace)
 
-p_configuration :: Configuration -> CharParser () Configuration
-p_configuration config = spaces *> comments *> p_section config
+p_configuration :: CharParser (String, Configuration) Configuration
+p_configuration = do
+        spaces *> comments *> many1 p_section
+        (_,config) <- getState
+        return config
 
-p_section :: Configuration -> CharParser () Configuration
-p_section config = do
+p_section :: CharParser (String, Configuration) [()]
+p_section = do
         section <- p_between '[' (many1 ch) ']'
-        let config' = updateSections section
-        config'' <- (spaces *> p_entries section config')
-        p_section (trace "Looping section" config'') <|> return (trace "Returning config from p_section" config'')
+        updateBoth $ updateSections section
+        case section of
+                "server" -> many p_server_entry
+                _        -> many p_site_entry
+        where
+                p_between left parser right = between (char left <* spaces) (char right) (parser <* spaces)
+                ch = satisfy (`notElem` "]\"\\")
+                updateSections "server" state           = state
+                updateSections section s@(site,config)  | section `elem` keys = s
+                                                        | site `elem` keys = s
+                                                        | otherwise = (site, config { configurationSites = M.insert section ds sites })
+                                 where
+                                        sites = configurationSites config
+                                        ds = configurationDefaultSite config
+                                        keys = M.keys sites
+
+p_server_entry :: CharParser (String, Configuration) ()
+p_server_entry = many serverSetting *> pure ()
+        where serverSetting =
+                    p_port
+                <|> p_ssl_port
+                <|> p_resource_per_req
+                <|> p_server_index
+                <|> p_server_passthrough
+                <|> p_site
+                <|> p_server_root
+                <?> "server setting"
+        
+p_site_entry :: CharParser (String, Configuration) ()
+p_site_entry = many serverSetting *> pure ()
+        where serverSetting =
+                    p_site_root
+                <|> p_site_index
+                <|> p_site_passthrough
+                <?> "site settings" 
+
+-- TODO: Add handling for default bare word
+p_port :: CharParser (String, Configuration) ()
+p_port = p_setting "port" (p_port' <|> p_port_list) *> pure ()
+        where
+             p_port' = do
+                port <- p_int
+                updateConfig $ \config -> config { configurationSinglePort = True, configurationPorts = [port] }
+             p_port_list = do
+                ports <- p_list p_int <?> "list of numbers"
+                updateConfig $ \config -> config { configurationSinglePort = False, configurationPorts = ports }
+                
+p_ssl_port :: CharParser (String, Configuration) ()
+p_ssl_port = p_setting "sslPort" p_ssl_port' *> pure ()
+        where
+                p_ssl_port' = do
+                        port <- p_int
+                        updateConfig $ \config -> config { configurationSSlPort = Just port }
+
+p_resource_per_req :: CharParser (String, Configuration) ()
+p_resource_per_req = p_setting "resourcePerRequest" p_resource_per_req' *> pure ()
+        where
+                p_resource_per_req' = do
+                        rpr <- p_bool
+                        updateConfig $ \config -> config { configurationResourcePerReq = rpr }
+
+p_index :: CharParser (String, Configuration) String
+p_index = p_setting "index" p_string
+
+p_server_index :: CharParser (String, Configuration) ()
+p_server_index = do
+        idx <- p_index
+        updateConfig $ \config -> config { configurationDefaultSite = configurationDefaultSite config `mappend` siteConfigurationIndex idx }
+        return ()
+
+-- TODO: Highlight this usage of pure in the blog entry part 2
+p_passthrough :: CharParser (String, Configuration) [String]
+p_passthrough = p_setting "passthrough" $ (pure <$> p_string) <|> p_list p_string
+
+p_server_passthrough :: CharParser (String, Configuration) ()
+p_server_passthrough = do
+        ss <- p_passthrough
+        updateConfig $ \config -> config { configurationDefaultSite = configurationDefaultSite config `mappend` siteConfigurationPassthrough ss }
+        return ()
+
+p_site :: CharParser (String, Configuration) ()
+p_site = p_setting "site" p_site' *> pure ()
+        where
+                p_site' = do
+                        s <- p_string
+                        updateSite $ \_ -> s
+
+p_root :: CharParser (String, Configuration) String
+p_root = p_setting "root" p_string
+
+p_server_root :: CharParser (String, Configuration) ()
+p_server_root = do
+        r <- p_root
+        updateConfig $ \config -> config { configurationDefaultSite = configurationDefaultSite config `mappend` siteConfigurationRoot r }
+        return ()
+
+p_site_root :: CharParser (String, Configuration) ()
+p_site_root = do
+        r <- p_root
+        updateBoth $ \(site,config) -> config { configurationSites = configurationSites config `mappend` m site r (configurationDefaultSite config) }
+        return ()
                 where
-                        p_between left parser right = between (char left <* spaces) (char right) (parser <* spaces)
-                        ch = satisfy (`notElem` "]\"\\")
-                        sites = configurationSites config
-                        ds = configurationDefaultSite config
-                        keys = M.keys sites
-                        updateSections "server" = config
-                        updateSections section  | section `elem` keys = config
-                                                | otherwise = if specialSiteName section `elem` keys
-                                                              then config
-                                                              else
-                                                                   let m = M.insert section ds sites
-                                                                   in config { configurationSites = m }
+                        m s r c = M.singleton s . siteConfigurationRoot . r' r $ c
+                        r' r c = root c ++ r
 
-
-p_entries :: String -> Configuration -> CharParser () Configuration
-p_entries section config = spaces *> comments *> p_entries' section config <?> "entries"
-
--- TODO: It's bad enough that this fails but the second problem is that I'm hand checking fields.  I should be making parsers for that.  Maybe that would let me drop this crazy recursive thing that isn't working
-p_entries' :: String -> Configuration -> CharParser () Configuration
-p_entries' section config = do
-        name <- trace "Reading field name now" fieldName
-        (trace (name ++ " pealing spaces off now") spaces *> char '=' *> spaces)
-        config' <- case (section, name) of
-                ("server", "port")                      -> trace (name ++ " Reading port now") $ p_port <|> p_port_list
-                ("server", "sslPort")                   -> trace (name ++ " Reading ssl port now") $ p_ssl_port
-                ("server", "resourcePerRequest")        -> trace (name ++ " Reading resource per now") $ p_resource_per_req
-                ("server", "index")                     -> trace (name ++ " Reading index now") $ p_index
-                ("server", "passthrough")               -> trace (name ++ " Reading passthrough now") $ p_passthrough <|> p_passthrough_list
-                ("server", "site")                      -> trace (name ++ " Reading site now") $ p_site
-                ("server", "root")                      -> trace (name ++ " Reading root now") $ p_root
-                (site, "root")                          -> trace (name ++ " Reading site root now") $ p_site_root site
-                (site, "index")                         -> trace (name ++ " Reading site index now") $ p_site_index site
-                (site, "passthrough")                   -> trace (name ++ " Reading site passthrough now") $ p_passthrough_site site <|> p_passthrough_site_list site
-                (site, field)                           -> trace (name ++ " Failing with unexpected now") $ unexpected $ "field " ++ field ++ " in section " ++ site
-        p_entries' (trace ("************Looping entries " ++ name) section) config' <|> return (trace "Return from entries config" config')
+p_site_index :: CharParser (String, Configuration) ()
+p_site_index = do
+        idx <- p_index
+        updateBoth $ \(site,config) -> config { configurationSites = configurationSites config `mappend` m site idx }
+        return ()
                 where
-                        defaultSite = configurationDefaultSite config
-                        p_port = do
-                                port <- p_int
-                                return config { configurationSinglePort = True, configurationPorts = [port] }
-                        p_list p = between (char '{' <* spaces) (char '}') $ (p <* spaces) `sepBy` (char ',' <* spaces)
-                        p_port_list = do
-                                ports <- p_list p_int <?> "list of numbers"
-                                return config { configurationSinglePort = False, configurationPorts = ports }
-                        p_ssl_port = do
-                                port <- p_int
-                                return config { configurationSSlPort = Just port }
-                        p_resource_per_req = do
-                                bool <- p_bool
-                                return config { configurationResourcePerReq = bool }
-                        p_index = do
-                                i <- p_string
-                                return config { configurationDefaultSite = defaultSite `mappend` SiteConfiguration mempty i mempty mempty }
-                        p_passthrough = do
-                                s <- p_string
-                                return config { configurationDefaultSite = defaultSite `mappend` SiteConfiguration mempty mempty [s] mempty }
-                        p_passthrough_list = do
-                                ss <- p_list p_string <?> "list of strings"
-                                return config { configurationDefaultSite = defaultSite `mappend` SiteConfiguration mempty mempty ss mempty }
-                        p_site = do
-                                site <- p_string
-                                let e = M.singleton (specialSiteName site) $ SiteConfiguration mempty mempty mempty mempty
-                                    sites = configurationSites config
-                                return config { configurationSites = sites `mappend` e }
-                        p_root = do
-                                r <- p_string
-                                return config { configurationDefaultSite = defaultSite `mappend` SiteConfiguration r mempty mempty mempty }
-                        p_site_root site = do
-                                r <- p_string
-                                let
-                                    r' = root defaultSite ++ r
-                                    e = M.singleton (deriveKey site) $ SiteConfiguration r' mempty mempty mempty
-                                    in return config { configurationSites = configurationSites config `mappend` e }
-                        p_site_index site = do
-                                i <- p_string
-                                let e = M.singleton (deriveKey site) $ SiteConfiguration mempty i mempty mempty
-                                return config { configurationSites = configurationSites config `mappend` e }
-                        p_passthrough_site site = do
-                                s <- p_string
-                                let e = M.singleton (deriveKey site) $ SiteConfiguration mempty mempty [s] mempty
-                                return config { configurationSites = configurationSites config `mappend` e }
-                        p_passthrough_site_list site = do
-                                ss <- p_list p_string
-                                let e = M.singleton (deriveKey site) $ SiteConfiguration mempty mempty ss mempty
-                                return config { configurationSites = configurationSites config `mappend` e }
-                        deriveKey site = let
-                                keys = M.keys $ configurationSites config
-                                ssn = specialSiteName site
-                                in if ssn `elem` keys then ssn else site
-                        fieldName = (:) <$> letter <*> many fieldChar
-                        fieldChar = letter <|> digit <|> oneOf "-_"
+                        m s i = M.singleton s . siteConfigurationIndex $ i
 
-p_comment :: CharParser () ()
+p_site_passthrough :: CharParser (String, Configuration) ()
+p_site_passthrough = do
+        ss <- p_passthrough
+        updateBoth $ \(site,config) -> config { configurationSites = configurationSites config `mappend` m site ss }
+        return ()
+                where
+                        m s ss = M.singleton s . siteConfigurationPassthrough $ ss
+
+p_comment :: CharParser (String, Configuration) ()
 p_comment = char '#' *> manyTill anyChar newline *> spaces *> pure () <?> "comment"
 
-comments :: CharParser () [()]
+comments :: CharParser (String, Configuration) [()]
 comments = many p_comment
 
-p_bool :: CharParser () Bool
+p_list :: CharParser (String, Configuration) a -> CharParser (String, Configuration) [a]
+p_list p = between (char '{' <* spaces) (char '}') $ (p <* spaces) `sepBy` (char ',' <* spaces)
+
+p_bool :: CharParser (String, Configuration) Bool
 p_bool = True <$ string "true"
      <|> False <$ string "false"
      <?> "boolean"
 
-p_int :: CharParser () Int
-p_int = pick <$> naturalOrFloat
-        where
-                pick (Left x) = fromIntegral x
-                pick (Right x) = truncate x
-                naturalOrFloat = P.naturalOrFloat lexer
-                lexer          = P.makeTokenParser emptyDef
+p_int :: CharParser (String, Configuration) Int
+p_int = read <$> many1 digit
 
-p_string :: CharParser () String
+p_string :: CharParser (String, Configuration) String
 p_string = between (char '\"') (char '\"') (many ch) <?> "string"
     where ch = char '\\' *> p_escape <|> satisfy (`notElem` "\"\\")
 
-p_escape :: CharParser () Char
+p_escape :: CharParser (String, Configuration) Char
 p_escape = choice (zipWith decode "\\\"/" "\\\"/")
     where decode c r = r <$ char c
+    
+-- helpers
 
-specialSiteName :: String -> String
-specialSiteName site = "DEFAULT{" ++ site ++ "}"
+p_setting :: String -> CharParser (String, Configuration) a -> CharParser (String, Configuration) a
+p_setting field p = string field *> spaces *> char '=' *> spaces *> p
+
+-- TODO: I think all of these are actually dropping the state and need to be fixed
+updateSite f = do
+        (site, config) <- getState
+        return (f site, config)
+        
+updateConfig f = do
+        (site, config) <- getState
+        return (site, f config)
+
+updateBoth f = do
+        state <- getState
+        return $ f state
+        
+-- Interface
 
 parseInput :: SourceName -> [Char] -> Configuration -> Either ParseError Configuration
-parseInput file input config = parse (p_configuration config) file input
+parseInput file input config = runParser p_configuration ("default", config) file input
 
 parseConfigFile :: FilePath -> Configuration -> IO (Either ParseError Configuration)
 parseConfigFile file config = do
