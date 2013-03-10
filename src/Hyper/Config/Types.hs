60699aa1bf963fae623a1be21aa5c4e40c24a9df
jason <jason@johnson-net.ch>
2013-03-10 21:55:16 +0100

    Config.Types: Added some helpers for making SiteConfigurations

--- src/Hyper/Config/Types.hs
+++ src/Hyper/Config/Types.hs
@@ -3,12 +3,17 @@ module Hyper.Config.Types
   SiteConfiguration(..)
 , ConfigurationStore(..)
 , Configuration(..)
+, siteConfigurationRoot
+, siteConfigurationIndex
+, siteConfigurationPassthrough
+, siteConfigurationCacheDirectory
 )
 where
 
 import           Data.Map    (Map, unionWith)
 import           Data.Monoid
 
+-- TODO: These names are too short, they need the site config prefix or they will clash with something
 data SiteConfiguration = SiteConfiguration {
           root           :: FilePath           -- TODO: For now we'll simply do file routing but we might let clients override this later
         , index          :: FilePath
@@ -58,3 +63,15 @@ mset :: (Eq a, Monoid a) => a -> a -> a
 mset a b | b == mempty = a
          | otherwise = b
 
+-- TODO: This stuff should be moved to Hyper.Config.Types.Helpers
+siteConfigurationRoot :: FilePath -> SiteConfiguration
+siteConfigurationRoot r = SiteConfiguration r mempty mempty mempty
+
+siteConfigurationIndex :: FilePath -> SiteConfiguration
+siteConfigurationIndex i = SiteConfiguration mempty i mempty mempty
+
+siteConfigurationPassthrough :: [String] -> SiteConfiguration
+siteConfigurationPassthrough ss = SiteConfiguration mempty mempty ss mempty
+
+siteConfigurationCacheDirectory :: FilePath -> SiteConfiguration
+siteConfigurationCacheDirectory c = SiteConfiguration mempty mempty mempty c
