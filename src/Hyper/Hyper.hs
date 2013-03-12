7a07304337d4287593535ee289062fa1a8f36c2f
jason <jason@johnson-net.ch>
2013-03-12 22:03:07 +0100

    Hyper main: initial working website (comments)
    The website has code to connect based on the configured port (but only
    one atm) and select the correct handler based on sigle site
    configuration or multi site.

--- src/Hyper/Hyper.hs
+++ src/Hyper/Hyper.hs
@@ -2,10 +2,29 @@ module Main where
 
 import           Hyper.Config       (loadConfiguration)
 import           Hyper.Options
+import           Hyper.Config.Types
 import           System.Environment (getArgs, getProgName)
+import Network.Wai.Handler.Warp (run)
+
+import qualified Hyper.Network.Wai.Handler.Single as S
+import qualified Hyper.Network.Wai.Handler.Multi as M
 
 main :: IO ()
 main = do
     progName <- getProgName
     config <- getArgs >>= parse progName >>= loadConfiguration
-    putStrLn $ show config
+    start (configurationSinglePort config) (configurationPorts config) config
+
+start :: Bool -> [Int] -> Configuration -> IO ()
+start True (port:_) config = do
+    putStrLn $ "http://localhost:" ++ show port ++ "/"
+    run port $ app multi
+        where
+            app True    = M.app sites defaultSite
+            app _       = S.app defaultSite
+            
+            multi = configurationMultiSite config
+            sites = configurationSites config
+            defaultSite = configurationDefaultSite config
+start _ ports config = do
+    error $ "Support for multiple ports currently disabled.  Ports: " ++ show ports ++ ", config: " ++ show config
