9acc5ef4d09ff9dd16c9251f7bc68580c1415d92
jason <jason@johnson-net.ch>
2013-03-14 10:44:57 +0100

    Hyper.Network.Wai.Handler: bug fix - fixed text type (comments)
    Html wasn't rendering because text type was wrong.  Now I have a hack in to just take the extension and apply that

--- src/Hyper/Network/Wai/Handler.hs
+++ src/Hyper/Network/Wai/Handler.hs
@@ -8,18 +8,19 @@ where
 
 import Network.Wai
 import Network.HTTP.Types
-import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Lazy.Char8 () -- Just for an orphan instance
 import           System.FilePath     (takeExtension, dropTrailingPathSeparator, (</>))
 
 import Hyper.Config.Types (SiteConfiguration)
 import qualified Hyper.Config.Types as T
+import           Data.ByteString.Char8      (ByteString, append, pack, unpack)
+import           Data.String                (IsString)
 
 isPassthrough :: FilePath -> SiteConfiguration -> Bool
 isPassthrough file = any (== (tail . takeExtension) file) . T.passthrough
 
 dispatchStaticFile :: String -> String -> Response
-dispatchStaticFile root file = ResponseFile status200 [("Content-Type", "text/html")] (root <\> file) Nothing
+dispatchStaticFile root file = ResponseFile status200 [textContentType file] (root <\> file) Nothing
 
 dispatchPlainFile :: String -> String -> Response
 dispatchPlainFile root file = ResponseFile status200 [("Content-Type", "text/plain")] (root <\> file) Nothing
@@ -39,3 +40,8 @@ dispatch site request = case rawPathInfo request of
 (<\>) :: FilePath -> FilePath -> FilePath
 a <\> b@('/':_) = dropTrailingPathSeparator a ++ b
 a <\> b = a </> b
+
+textContentType :: IsString t => FilePath -> (t, ByteString)
+textContentType file = ("Content-Type", append "text/" t)
+    where
+        t = pack . tail . takeExtension $ file
