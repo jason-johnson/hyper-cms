9c34a49afd006587047e443ef08327abb4e9420f
jason <jason@johnson-net.ch>
2013-03-16 23:53:41 +0100

    TemplateEngine: Made apply command do the template apply (comments)
    It seems to be working but the new problem is that it goes into an endless loop.  I suspect it's because exceptions don't always break out so it probably just goes over and over.

--- src/Hyper/Text/TemplateEngine.hs
+++ src/Hyper/Text/TemplateEngine.hs
@@ -28,10 +28,10 @@ commands = M.fromList [
 
 -- TODO: most of these exceptions aren't actually being thrown.  That will have to be dealt with at some point
 processContents :: ByteString -> (ByteString -> TemplateState -> IO TemplateState) -> TemplateState -> IO TemplateState
-processContents contents write vars = do
+processContents contents write state = do
     let (static, command) = B8.breakSubstring "<hyper:" contents
-    vars' <- write static vars
-    parseCommand command write vars'
+    state' <- write static state
+    parseCommand command write state'
 
 parseCommand :: ByteString -> (ByteString -> TemplateState -> IO TemplateState) -> TemplateState -> IO TemplateState
 parseCommand "" _ vars = return vars
@@ -42,7 +42,7 @@ parseCommand comm write state = let (command, rest) = breakCommand comm in
             (args, rest) = breakEndTag a
             (content, rest') = breakCloseTag c rest in
             (processArgs c args, content, dropCommand c rest')
-        processArgs com = toPairs com . Prelude.filter (not . B8.null) . B8.splitWith (\c -> c == ' ' || c == '=')
+        processArgs com = toPairs com . Prelude.filter (not . B8.null) . B8.splitWith (\c -> c == ' ' || c == '=' || c == '"')
         breakCommand = B8.break (== ' ') . B8.drop 7
         breakEndTag = B8.break (== '>')
         breakCloseTag c = B8.breakSubstring ("</hyper:" `B8.append` c `B8.append` ">") . B8.drop 1
@@ -57,12 +57,13 @@ parseCommand comm write state = let (command, rest) = breakCommand comm in
             where
                 parseFail = error $ "parse fail: templied called undefined command: '" ++ B8.unpack c ++ "'"
 
-applyTemplate :: FilePath -> TemplateState -> IO ()
+applyTemplate :: FilePath -> TemplateState -> IO TemplateState
 applyTemplate template (root, current, prev, vars) = do
     path <- findFile current'
     c <- B8.readFile path
     state' <- processContents c write (root, current, path, vars)
     B8.hPutStrLn stderr $ "state is: " `B8.append` B8.pack (show state')
+    return state'
     where
         findFile c = do
             let path = root </> c </> template
@@ -87,8 +88,9 @@ commandApply :: CommandArgs -> ByteString -> TemplateState -> IO TemplateState
 commandApply args content state = do
     let template = tryAttrLookup "apply" "template" args
     s' <- processContents content write state
+    s'' <- applyTemplate (B8.unpack template) s'
     return $
-        s'
+        s''
         where
             write "" s = return s
             write s (r, c, l, v)  = return $ (r, c, l, M.insertWith (flip B8.append) Content s v)
